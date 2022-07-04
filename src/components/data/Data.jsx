538a8fe42d8fa2e2b48e789f25d83ed404a3ca06
Gustavo Baranda <baranda.gustavo@gmail.com>
2022-07-03 22:39:26 -0300

    correcion

--- src/components/data/Data.jsx
+++ src/components/data/Data.jsx
@@ -14,28 +14,36 @@ export const  data =
         "nombre":"McFiesta",
         "precio": 630,
         "imagen": "./img/mcfiesta.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Combos Hambuerguesas" 
     },
     {
         "id":3,
         "nombre":"McDuo",
         "precio": 550,
         "imagen": "./img/mcDuo.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Combos Hambuerguesas" 
     },
     {
         "id":4,
         "nombre":"Cuarto de libra con queso",
         "precio": 880,
         "imagen": "./img/cuartoDeLibra.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Combos Hambuerguesas"
     },
     {
         "id":5,
         "nombre":"Doble Cuarto de libra con queso",
         "precio": 1040,
         "imagen": "./img/dobleCuartoDeLibra.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Combos Hambuerguesas" 
     },
     {
         "id":6,
@@ -49,98 +57,126 @@ export const  data =
         "nombre":"Triple Hamburguesa con queso",
         "precio": 1030,
         "imagen": "./img/tripleHamburguesaConQueso.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Combos Hambuerguesas" 
     },
     {
         "id":8,
         "nombre":"Hamburguesa Con Queso",
         "precio": 350,
         "imagen": "./img/hamburguesaConQueso.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Combos Hambuerguesas" 
     },
     {
         "id":9,
         "nombre":"McPollo",
         "precio": 580,
         "imagen": "./img/mcPollo.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Combos Hambuerguesas" 
     },
     {
         "id":10,
         "nombre":"Papas con cheddar & bacon",
         "precio": 510,
         "imagen": "./img/papasconCheddar&Bacon.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Acompañamiento" 
     },
     {
         "id":11,
         "nombre":"Side salad",
         "precio": 450,
         "imagen": "./img/sideSalad.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Acompañamiento" 
     },
     {
         "id":12,
         "nombre":"Coca cola 500ml",
         "precio": 450,
         "imagen": "./img/cocaCola.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Bebidas" 
     },
     {
         "id":13,
         "nombre":"fanta 500ml",
         "precio": 450,
         "imagen": "./img/fanta.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Bebidas" 
     },
     {
         "id":14,
         "nombre":"jugo de manzana Cepita 500ml",
         "precio": 350,
         "imagen": "./img/jugoDeManzana.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Bebidas" 
     },
     {
         "id":15,
         "nombre":"Agua sin gas 500ml",
         "precio": 580,
         "imagen": "./img/aguaSinGas500ml.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Bebidas" 
     },
     {
         "id":16,
         "nombre":"Cerveza Patagonia Amber 750ml",
         "precio": 650,
         "imagen": "./img/patagoniaAmber.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Bebidas"  
     },
     {
         "id":17,
         "nombre":"Cerveza Patagonia Kune 750ml",
         "precio": 650,
         "imagen": "./img/patagoniaKune.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Bebidas"  
     },
     {
         "id":18,
         "nombre":"Cerveza Patagonia Weisse 750ml",
         "precio": 650,
         "imagen": "./img/patagoniaWeisse.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Bebidas"  
     },
     {
         "id":19,
         "nombre":"Sundae frutilla",
         "precio": 350,
         "imagen": "./img/sundaefrutilla.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Postre"  
     },
     {
         "id":20,
         "nombre":"McFlurry Oreo",
         "precio": 580,
         "imagen": "./img/mcFlurryOreo.png",
-        "stock": 20 
+        "stock": 20,
+        "descripcion": "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único.",
+        "categoria": "Postre"  
     }
 ]
 
