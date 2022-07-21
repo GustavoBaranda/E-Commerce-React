0ce51db96e22fca3fd718af9fd8ec87adad7d887
Gustavo Baranda <baranda.gustavo@gmail.com>
2022-07-20 21:22:19 -0300

    correccion

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import './services/config';
+import './services/firestore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
